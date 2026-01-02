9fce58cfb685079a306ad4254452fcdbab6c296d
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:07:53 +0700

    Menambahkan class EntryViewModel untuk mengelola state input data siswa

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+}
