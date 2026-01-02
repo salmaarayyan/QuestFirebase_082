79423595eb4a29f841a3a10c7a622b5477564b7d
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:19:38 +0700

    Menambahkan implementasi HostNavigasi untuk pengaturan rute navigasi aplikasi

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -3,9 +3,30 @@ package com.example.myfirebase.view.controlNavigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.view.route.DestinasiEntry
+import com.example.myfirebase.view.route.DestinasiHome
 
 @Composable
 fun DataSiswaApp(navController: NavController = rememberNavController(), modifier: Modifier = Modifier) {
     HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)})
+        }
+    }
 }
