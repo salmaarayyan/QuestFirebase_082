01a29911a1150b0bb3cc101ff960ba79a2fbafbd
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:53:55 +0700

    Menambahkan konfigurasi NavHost dan routing untuk halaman Home dan Entry Siswa

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -2,21 +2,24 @@ package com.example.myfirebase.view.controlNavigasi
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myfirebase.view.EntrySiswaScreen
+import com.example.myfirebase.view.HomeScreen
 import com.example.myfirebase.view.route.DestinasiDetail
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.view.route.DestinasiHome
 
 @Composable
-fun DataSiswaApp(navController: NavController = rememberNavController(), modifier: Modifier = Modifier) {
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
     HostNavigasi(navController = navController)
 }
 
 @Composable
 fun HostNavigasi(
-    navController: NavController,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
