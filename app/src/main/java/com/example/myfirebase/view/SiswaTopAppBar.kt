6a2e57580f155aadb9743772848b793f8eef6ee0
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:27:22 +0700

    Menambahkan komponen SiswaTopAppBar untuk navigasi aplikasi

---
+++ app/src/main/java/com/example/myfirebase/view/SiswaTopAppBar.kt
@@ -0,0 +1,16 @@
+package com.example.myfirebase.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){}
