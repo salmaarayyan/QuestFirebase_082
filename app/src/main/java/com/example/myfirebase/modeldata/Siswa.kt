15a0312f4dac5d2f8c9abb9d799178bc3958cd04
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 18:20:39 +0700

    Menambahkan data class UIStateSiswa dan fungsi ekstensi toUiStateSiswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -16,3 +16,13 @@ data class DetailSiswa(
 
 fun DetailSiswa.toSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
