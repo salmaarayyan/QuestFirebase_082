d4de9880d93ffdc9c6ee96c707e08573297b6e67
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:03:54 +0700

    Menambahkan class HomeViewModel untuk mengelola state data siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
 }
