a33fe655f4faad66501e0c0504c113900126e876
salmaarayyan <salmaarayyan@gmail.com>
2026-01-02 19:05:29 +0700

    Menambahkan logika pengambilan data siswa pada HomeViewModel menggunakan coroutine

--- app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.myfirebase.viewmodel
 
+import android.net.http.HttpException
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myfirebase.modeldata.Siswa
 import com.example.myfirebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
@@ -19,5 +23,18 @@ class HomeViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() 
 
     init {
         loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositoriSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
     }
 }
