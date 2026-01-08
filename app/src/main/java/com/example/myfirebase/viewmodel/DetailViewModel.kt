c9cdb984f4a62880ec72f30d630e878113a81be2
salmaarayyan <salmaarayyan@gmail.com>
2026-01-08 11:09:34 +0700

    Menambahkan class DetailViewModel dengan dependency RepositorySiswa dan SavedStateHandle

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -3,6 +3,9 @@ package com.example.myfirebase.viewmodel
 
 import com.example.myfirebase.modeldata.Siswa
 import kotlinx.serialization.InternalSerializationApi
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.repositori.RepositorySiswa
 
 
 sealed interface StatusUIDetail {
@@ -11,3 +14,9 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 }
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+}
